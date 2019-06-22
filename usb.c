54119292cea56606e480a7a357ed55e01c5b5ed9
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2019-06-22 11:27:10 +0200

    add known USB ID's for RTL8822BU and RTL8822CU
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- usb.c
+++ usb.c
@@ -21,7 +21,29 @@ static void rtw_usb_disconnect(struct usb_interface *intf)
 {
 }
 
+#define USB_VENDOR_ID_REALTEK           0x0bda
+#define USB_VENDOR_ID_EDIMAX		0x7392
+
 static const struct usb_device_id rtw_usb_id_table[] = {
+#ifdef CONFIG_RTW88_8822BU
+	{RTK_USB_DEVICE_AND_INTERFACE(USB_VENDOR_ID_REALTEK, 0xB82C, 0xff, 0xff, 0xff, rtw8822b_hw_spec) }, /* Default ID for USB multi-function */
+	{RTK_USB_DEVICE_AND_INTERFACE(USB_VENDOR_ID_REALTEK, 0xB812, 0xff, 0xff, 0xff, rtw8822b_hw_spec) }, /* Default ID for USB Single-function, WiFi only */
+	{RTK_USB_DEVICE_AND_INTERFACE(USB_VENDOR_ID_EDIMAX, 0xB822, 0xff, 0xff, 0xff, rtw8822b_hw_spec) }, //EDX
+	{RTK_USB_DEVICE_AND_INTERFACE(USB_VENDOR_ID_EDIMAX, 0xC822, 0xff, 0xff, 0xff, rtw8822b_hw_spec) }, //EDX
+	{RTK_USB_DEVICE(0x0b05, 0x184c, rtw8822b_hw_spec) },	/* ASUS AC53 Nano */
+	{RTK_USB_DEVICE(0x0b05, 0x1841, rtw8822b_hw_spec) },	/* ASUS AC55 B1 */
+	{RTK_USB_DEVICE(0x2001, 0x331c, rtw8822b_hw_spec) },	/* D-Link DWA-182 rev D1 */
+	{RTK_USB_DEVICE(0x13b1, 0x0043, rtw8822b_hw_spec) },	/* Linksys WUSB6400M */
+#endif
+
+#ifdef CONFIG_RTW88_8822CU
+	{RTK_USB_DEVICE_AND_INTERFACE(USB_VENDOR_ID_REALTEK, 0xb82b, 0xff, 0xff, 0xff, rtw8822c_hw_spec) }, /* 8821CU */
+	{RTK_USB_DEVICE_AND_INTERFACE(USB_VENDOR_ID_REALTEK, 0xb820, 0xff, 0xff, 0xff, rtw8822c_hw_spec) }, /* 8821CU */
+	{RTK_USB_DEVICE_AND_INTERFACE(USB_VENDOR_ID_REALTEK, 0xC821, 0xff, 0xff, 0xff, rtw8822c_hw_spec) }, /* 8821CU */
+	{RTK_USB_DEVICE_AND_INTERFACE(USB_VENDOR_ID_REALTEK, 0xC820, 0xff, 0xff, 0xff, rtw8822c_hw_spec) }, /* 8821CU */
+	{RTK_USB_DEVICE_AND_INTERFACE(USB_VENDOR_ID_REALTEK, 0xC82A, 0xff, 0xff, 0xff, rtw8822c_hw_spec) }, /* 8821CU */
+	{RTK_USB_DEVICE_AND_INTERFACE(USB_VENDOR_ID_REALTEK, 0xC82B, 0xff, 0xff, 0xff, rtw8822c_hw_spec) }, /* 8821CU */
+#endif
 	{},
 };
 MODULE_DEVICE_TABLE(usb, rtw_usb_id_table);
--- usb.h
+++ usb.h
@@ -3,4 +3,13 @@
 #ifndef __RTK_USB_H_
 #define __RTK_USB_H_
 
+
+#define RTK_USB_DEVICE(vend, dev, hw_config)	\
+	USB_DEVICE(vend, dev),			\
+	.driver_info = (kernel_ulong_t) & (hw_config),
+
+#define RTK_USB_DEVICE_AND_INTERFACE(vend, dev, cl, sc, pr, hw_config)	\
+	USB_DEVICE_AND_INTERFACE_INFO(vend, dev, cl, sc, pr),		\
+	.driver_info = (kernel_ulong_t) & (hw_config),
+
 #endif
